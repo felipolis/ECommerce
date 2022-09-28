abd7efa6cb51c80a587dbdefe08707e1ceeaa4fe
Felipe Archanjo <felipecunhamendes@gmail.com>
2022-09-28 19:52:20 -0300

    Criando o CartComponent

---
+++ context/StateContext.js
@@ -0,0 +1,121 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+	const [showCart, setShowCart] = useState(false);
+	const [cartItems, setCartItems] = useState([]);
+	const [totalPrice, setTotalPrice] = useState(0);
+	const [totalQuantities, setTotalQuantities] = useState(0);
+	const [qty, setQty] = useState(1);
+
+	let foundProduct;
+	let index;
+
+	const onAdd = (product, quantity) => {
+		const checkProductInCart = cartItems.find((item) => item._id === product._id);
+
+		setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+		setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+		if (checkProductInCart) {
+
+			const updatedCartItems = cartItems.map((cartProduct) => {
+				if (cartProduct._id === product._id) {
+					return {
+						...cartProduct,
+						quantity: cartProduct.quantity + quantity,
+					};
+				} else {
+					return cartProduct;
+				}
+			});
+
+			setCartItems(updatedCartItems);
+		} else{
+			
+			product.quantity = quantity;
+			setCartItems([...cartItems, { ...product }]);	
+			
+		}
+		
+		toast.success(`${qty} ${product.name} added to cart`);
+
+	}
+
+	const onRemove = (product) => {
+		foundProduct = cartItems.find((item) => item._id === product._id);
+		index = cartItems.indexOf(foundProduct);
+
+		setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+		setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity);
+		setCartItems([...cartItems.slice(0, index), ...cartItems.slice(index + 1)]);
+		toast.error(`${foundProduct.name} removed from cart`);
+	}
+
+	const toggleCartItemQuantity = (id, value) => {
+		foundProduct = cartItems.find((item) => item._id === id);
+		index = cartItems.findIndex((product) => product._id === id);
+
+
+		if(value === 'inc'){
+
+			let newCartItems = [...cartItems];
+			newCartItems[index].quantity = foundProduct.quantity + 1;
+			setCartItems(newCartItems);
+			
+			setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+			setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+			
+			
+		} else if(value === 'dec'){
+			
+			if(foundProduct.quantity > 1){
+
+				let newCartItems = [...cartItems];
+				newCartItems[index].quantity = foundProduct.quantity - 1;
+				setCartItems(newCartItems);
+				
+	
+				setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+				setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+
+			}
+
+		}
+	}
+
+	const incQty = (prevQty) => {
+		setQty((prevQty) => prevQty + 1);
+	};
+
+	const decQty = (prevQty) => {
+		setQty((prevQty) => {
+			if (prevQty - 1 < 1) return 1;
+
+			return prevQty - 1;
+		});
+	};
+
+	return (
+		<Context.Provider
+			value={{
+				showCart,
+				setShowCart,
+				cartItems,
+				totalPrice,
+				totalQuantities,
+				qty,
+				incQty,
+				decQty,
+				onAdd,
+				toggleCartItemQuantity,
+				onRemove,
+			}}
+		>
+			{children}
+		</Context.Provider>
+	);
+}
+
+export const useStateContext = () => useContext(Context);
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,59 +3,61 @@ import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import { Product } from '../../components'
+import { useStateContext } from '../../context/StateContext'
 
 const ProductDetails = ({ product, products }) => {
 	const { image, name, details, price } = product
 	const [index, setIndex] = useState(0)
+	const { qty, incQty, decQty, onAdd } = useStateContext()
 
-  return (
-    <div>
+	return (
+		<div>
 			<div className='product-detail-container'>
-					<div>
-						<div className='image-container'>
-							<img src={urlFor(image && image[index])} className="product-detail-image"/>
-						</div>
-						<div className='small-images-container'>
-							{image?.map((item, i) => (
-								<img 
-									src={urlFor(item)} 
-									className={i === index ? 'small-image selected-image' : 'small-image'}
-									onMouseEnter={() => setIndex(i)}
-								/>
-							))}
-						</div>
+				<div>
+					<div className='image-container'>
+						<img src={urlFor(image && image[index])} className="product-detail-image" />
 					</div>
+					<div className='small-images-container'>
+						{image?.map((item, i) => (
+							<img
+								src={urlFor(item)}
+								className={i === index ? 'small-image selected-image' : 'small-image'}
+								onMouseEnter={() => setIndex(i)}
+							/>
+						))}
+					</div>
+				</div>
 
-					<div className='product-detail-desc'>
-						<h1>{name}</h1>
-						<div className='reviews'>
-							<div>
-								<AiFillStar />
-								<AiFillStar />
-								<AiFillStar />
-								<AiFillStar />
-								<AiOutlineStar />
-							</div>
-							<p>
-								(20)
-							</p>
+				<div className='product-detail-desc'>
+					<h1>{name}</h1>
+					<div className='reviews'>
+						<div>
+							<AiFillStar />
+							<AiFillStar />
+							<AiFillStar />
+							<AiFillStar />
+							<AiOutlineStar />
 						</div>
-						<h4>Details:</h4>
-						<p>{details}</p>
-						<p className='price'>${price}</p>
-						<div className='quantity'>
-							<h3>Quantity:</h3>
-							<p className='quantity-desc'>
-								<span className='minus' onClick=""><AiOutlineMinus /></span>
-								<span className='num' onClick="">0</span>
-								<span className='plus' onClick=""><AiOutlinePlus /></span>
-							</p>
-						</div>
-						<div className='buttons'>
-							<button type='button' className='add-to-cart'>Add to Cart</button>
-							<button type='button' className='buy-now' onClick="">Buy Now</button>
-						</div>
+						<p>
+							(20)
+						</p>
 					</div>
+					<h4>Details:</h4>
+					<p>{details}</p>
+					<p className='price'>${price}</p>
+					<div className='quantity'>
+						<h3>Quantity:</h3>
+						<p className='quantity-desc'>
+							<span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+							<span className='num'>{qty}</span>
+							<span className='plus' onClick={incQty}><AiOutlinePlus /></span>
+						</p>
+					</div>
+					<div className='buttons'>
+						<button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to Cart</button>
+						<button type='button' className='buy-now' onClick="">Buy Now</button>
+					</div>
+				</div>
 			</div>
 
 			<div className='maylike-products-wrapper'>
@@ -63,7 +65,7 @@ const ProductDetails = ({ product, products }) => {
 				<div className='marquee'>
 					<div className='maylike-products-container track'>
 						{products.map((item) => (
-							<Product 
+							<Product
 								key={item._id}
 								product={item}
 							/>
@@ -71,8 +73,8 @@ const ProductDetails = ({ product, products }) => {
 					</div>
 				</div>
 			</div>
-    </div>
-  )
+		</div>
+	)
 }
 
 export const getStaticPaths = async () => {
@@ -85,8 +87,8 @@ export const getStaticPaths = async () => {
 	const products = await client.fetch(query)
 
 	const paths = products.map((product) => ({
-		params: { 
-			slug: product.slug.current 
+		params: {
+			slug: product.slug.current
 		},
 	}))
 
@@ -96,19 +98,19 @@ export const getStaticPaths = async () => {
 	}
 }
 
-export const getStaticProps = async ({ params: { slug }}) => {
-  const query = `*[_type == "product" && slug.current == "${slug}"][0]`
+export const getStaticProps = async ({ params: { slug } }) => {
+	const query = `*[_type == "product" && slug.current == "${slug}"][0]`
 	const productsQuery = `*[_type == "product"]`
 
-  const product = await client.fetch(query)
+	const product = await client.fetch(query)
 	const products = await client.fetch(productsQuery)
 
-  return {
-    props: {
-      products,
-      product
-    }
-  }
+	return {
+		props: {
+			products,
+			product
+		}
+	}
 }
 
 export default ProductDetails
